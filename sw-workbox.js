3878eeddcf4016524bfe7751a8a9f4185fcd7ed4
DagProgs <114062397+DagProgs@users.noreply.github.com>
2026-03-24 10:24:06 +0300

    update

--- sw-workbox.js
+++ sw-workbox.js
@@ -53,6 +53,42 @@ workbox.precaching.precacheAndRoute([
     "revision": "db409cd90d613a43e7a19c449e074441"
   },
   {
+    "url": "assets/icons/icon-128x128.png",
+    "revision": "c1fdb4f328dc224c31d4f63b37a2d7eb"
+  },
+  {
+    "url": "assets/icons/icon-144x144.png",
+    "revision": "fbf62408a29bafea9714e5006212cdbc"
+  },
+  {
+    "url": "assets/icons/icon-152x152.png",
+    "revision": "480c47876f2576e4ea51c6e6f6b3a0e1"
+  },
+  {
+    "url": "assets/icons/icon-192x192.png",
+    "revision": "661e7275a650d6a3616d38e98bd09219"
+  },
+  {
+    "url": "assets/icons/icon-384x384.png",
+    "revision": "15230a15012db5dfc25c8756191f8ded"
+  },
+  {
+    "url": "assets/icons/icon-48x48.png",
+    "revision": "34950ff63a4a4de30783fa922faee9c2"
+  },
+  {
+    "url": "assets/icons/icon-512x512.png",
+    "revision": "28dca3664bb91e23c7a8ad2d0d8194d7"
+  },
+  {
+    "url": "assets/icons/icon-72x72.png",
+    "revision": "bcd59f0dc93d37c8e8f963471f39028d"
+  },
+  {
+    "url": "assets/icons/icon-96x96.png",
+    "revision": "cce9088080585a31862e0fe7d70d306f"
+  },
+  {
     "url": "assets/img/1.png",
     "revision": "ebd6b3b3e70a8b2a01f3485fdf0199c3"
   },
